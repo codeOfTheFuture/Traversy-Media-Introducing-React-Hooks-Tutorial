b1d44cfdef83b58b506ddb3075466c12d5b32f2c
Jeff Oliver <jeffoliver3384@gmail.com>
2019-08-04 14:34:21 -0400

    mapped through list of todos in state

--- react_hooks_todos/src/App.js
+++ react_hooks_todos/src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: 'Learn about React',
+      isCompleted: false,
+    },
+    {
+      text: 'Meet friend for lunch',
+      isCompleted: false,
+    },
+    {
+      text: 'Build really cool todo app',
+      isCompleted: false,
+    },
+  ]);
+
   return (
-    <div className='App'>
-      <h1>Todo</h1>
+    <div className='app'>
+      <dive className='todo-list'>
+        {todos.map((todo, index) => {
+          <Todo key={index} index={idnex} todo={todo} />;
+        })}
+      </dive>
     </div>
   );
 }
