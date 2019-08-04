516ac4e989fe801e33365714a01d8e90f0131728
Jeff Oliver <jeffoliver3384@gmail.com>
2019-08-04 14:59:16 -0400

    created todo form to add todo with hook to store input value in state and setvalue after form submit and method to add todo to state

--- react_hooks_todos/src/App.js
+++ react_hooks_todos/src/App.js
@@ -1,6 +1,33 @@
 import React, { useState } from 'react';
 import './App.css';
 
+function Todo({ todo, index }) {
+  return <div className='todo'>{todo.text}</div>;
+}
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState('');
+
+  const handleSumbit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue('');
+  };
+
+  return (
+    <form onSubmit={handleSumbit}>
+      <input
+        type='text'
+        className='input'
+        value={value}
+        placeholder='Add Todo'
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
 function App() {
   const [todos, setTodos] = useState([
     {
@@ -17,13 +44,19 @@ function App() {
     },
   ]);
 
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
   return (
     <div className='app'>
-      <dive className='todo-list'>
-        {todos.map((todo, index) => {
-          <Todo key={index} index={idnex} todo={todo} />;
-        })}
-      </dive>
+      <div className='todo-list'>
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
     </div>
   );
 }
